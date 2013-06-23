34467096d24256b14699ae6b50bf260a5d75580b
wumingli <wml_boy0543@163.com>
2013-06-23 15:08:43 +0800

    update time plugi

--- modules/timeYMPlugin/js/timeYMPlugin.js
+++ modules/timeYMPlugin/js/timeYMPlugin.js
@@ -7,7 +7,11 @@
 */
 (function ($){
     var yearPage = 0, curPage = 0;
-    var timeData = function (st, et, type){
+    /*返回时间数据
+     st:开始年份/月份
+     et:结束年份/月份,
+    */
+    function getTimeData(st, et, type){
         var start = st || 1949,
             end = et || 2013;
         if (type && type == 'month'){
@@ -30,6 +34,7 @@
         }
         return timeArray;
     }
+    //生成控件
     function createTimeBox($target, type, cfg){
         $('.timeYM_box').remove();
         var $timeBox = $('<div />'),
@@ -55,66 +60,65 @@
             left: targetOffset.left,
             top: targetOffset.top + $target.height()
         });
-        //Box不存在，则创建
-        if ($(cfg.name + '_box').length == 0){
-            if (cfg.type == 'month'){
-                //开始月份，开始年份和结束年份相同
-                if(cfg.name == '#start_date_m' && $('#start_date_y').val() != '' && 
-                    $('#start_date_y').val() == $('#end_date_y').val() && $('#end_date_y').val() != ''){
-                    timeArray = timeData(1, $('#end_date_m').val(), 'month');
-                }
-                else if(cfg.name == '#end_date_m' && $('#start_date_y').val() != '' && 
-                    $('#start_date_y').val() == $('#end_date_y').val() && $('#start_date_m').val() != ''){
-                    timeArray = timeData($('#start_date_m').val(), 12, 'month');
-                }
-                else{
-                    timeArray = timeData(1, 12, 'month');
-                }
-                titHtml = '<div class="tit">请选择' + cfg.title + '</div>';
-                timeSort = '月';
+        //月份列表
+        if (cfg.type == 'month'){
+            //开始月份，开始年份和结束年份相同
+            if(cfg.name == '#start_date_m' && $('#start_date_y').val() != '' && 
+                $('#start_date_y').val() == $('#end_date_y').val() && $('#end_date_y').val() != ''){
+                timeArray = getTimeData(1, $('#end_date_m').val(), 'month');
             }
-            else {
-                if(cfg.name == '#start_date_y' && $('#end_date_y').val() != ''){
-                    timeArray = timeData(1949, $('#end_date_y').val(), 'year');
-                }
-                else if(cfg.name == '#end_date_y' && $('#start_date_y').val() != ''){
-                    timeArray = timeData($('#start_date_y').val(), 2013, 'year');
-                }
-                else{
-                    timeArray = timeData(1949, 2013, 'year');
-                }
-                titHtml = '<div class="tit"><em class="prev disable">&lt;&lt; </em>请选择' + cfg.title + 
-                        '<em class="next"> &gt;&gt;</em></div>';
+            else if(cfg.name == '#end_date_m' && $('#start_date_y').val() != '' && 
+                $('#start_date_y').val() == $('#end_date_y').val() && $('#start_date_m').val() != ''){
+                timeArray = getTimeData($('#start_date_m').val(), 12, 'month');
             }
-            titHtml += '<div class="timeCon">';
-            titHtml += '<div class="oneBlock">';
-            for (var i=0, len = timeArray.length; i < len ; i++){
-                titHtml += '<span>' + timeArray[i] + timeSort + '</span> ';
-                if ((i + 1) % 12 == 0){
-                    titHtml += '</div>';
-                    titHtml += '<div class="oneBlock">'
-                }
+            else{
+                timeArray = getTimeData(1, 12, 'month');
             }
-            titHtml += '</div>';
-            titHtml += '</div>';
-            //清空
-            if (cfg.name == '#end_date_y' || cfg.name == '#end_date_m' ){
-                sinceNowStr = '<a class="btnSinceNow" href="javascript:void(0);">至今</a> ';
-            }
-            titHtml += '<div class="btns">' + sinceNowStr + '<a href="javascript:void(0);" class="clear">清空</a>' + 
-                    ' <a href="javascript:void(0);" class="btnClose">关闭</a></div>';
-            $timeBox.append($(titHtml));
-            $('body').append($timeBox);
+            titHtml = '<div class="tit">请选择' + cfg.title + '</div>';
+            timeSort = '月';
         }
-        //$(cfg.name + '_box').show().siblings('.timeYM_box').hide();
+        //年份列表
+        else {
+            if(cfg.name == '#start_date_y' && $('#end_date_y').val() != ''){
+                timeArray = getTimeData(1949, $('#end_date_y').val(), 'year');
+            }
+            else if(cfg.name == '#end_date_y' && $('#start_date_y').val() != ''){
+                timeArray = getTimeData($('#start_date_y').val(), 2013, 'year');
+            }
+            else{
+                timeArray = getTimeData(1949, 2013, 'year');
+            }
+            titHtml = '<div class="tit"><em class="prev disable">&lt;&lt; </em>请选择' + cfg.title + 
+                    '<em class="next"> &gt;&gt;</em></div>';
+        }
+        titHtml += '<div class="timeCon">';
+        titHtml += '<div class="oneBlock">';
+        for (var i=0, len = timeArray.length; i < len ; i++){
+            titHtml += '<span>' + timeArray[i] + timeSort + '</span> ';
+            if ((i + 1) % 12 == 0){
+                titHtml += '</div>';
+                titHtml += '<div class="oneBlock">'
+            }
+        }
+        titHtml += '</div>';
+        titHtml += '</div>';
+        //清空
+        if (cfg.name == '#end_date_y' || cfg.name == '#end_date_m' ){
+            sinceNowStr = '<a class="btnSinceNow" href="javascript:void(0);">至今</a> ';
+        }
+        titHtml += '<div class="btns">' + sinceNowStr + '<a href="javascript:void(0);" class="clear">清空</a>' + 
+                ' <a href="javascript:void(0);" class="btnClose">关闭</a></div>';
+        $timeBox.append($(titHtml));
+        $('body').append($timeBox);
     }
     $.fn.timeYM = function (options){
         $this = $(this);
         var config = {
             name: '#' + $this.attr('id'),
             title: '开始年份',
-            next: '#start_date_m',
             next: '开始月份',
+            next: '#start_date_m',
+            minYear: 1949,
             method: 'click',
             type: 'year'
         };
@@ -128,16 +132,18 @@
                 }
                 return true;
             });
-            createTimeBox($(timePlugin.name), timePlugin.type, timePlugin);
+            if($(timePlugin.name + '_box').length == 0){
+                createTimeBox($(timePlugin.name), timePlugin.type, timePlugin);
+            }
         });
     }
     $(document).click(function (){
-        $('.timeYM_box').hide();
+        $('.timeYM_box').remove();
     });
     $('.timeYM_box').live('click', function (event){
         (event || window.event).stopPropagation();
     });
-    //为年份左右箭头添加事件
+    //左箭头点击事件
     $('.timeYM_box .tit em.prev').live('click', function (){
         if (curPage > 0 && !$('.timeYM_box .timeCon').is(':animated')){
             curPage--;
@@ -151,6 +157,7 @@
             });
         }
     });
+    //右箭头点击事件
     $('.timeYM_box .tit em.next').live('click', function (){
         if ((curPage + 1) < yearPage && !$('.timeYM_box .timeCon').is(':animated')){
             curPage++;
@@ -170,7 +177,6 @@
         var theTimeTxt = $theBox.attr('data-box-for');        
         $('#' + theTimeTxt).val($(this).text().replace(/年|月/,''));
         $(this).addClass('cur').siblings().removeClass('cur');
-        $theBox.remove();
         switch (theTimeTxt){
             case 'start_date_y':
                 if($('#start_date_m').val() == ''){
@@ -187,10 +193,8 @@
                     $('#end_date_m').trigger('click').focus();
                 }
                 break;
-            default:
-                break;
         }
-        /**/
+        $theBox.remove();
     });
     //清空
     $('.timeYM_box .btns .clear').live('click', function (){
